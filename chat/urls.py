8f6efeee04f7f80721db86b62cebe16900a900ae
Zzzzzzzzzz127 <1536451651@qq.com>
2025-07-03 11:55:19 +0800

    聊天室功能

--- chat/urls.py
+++ chat/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('room/data/<int:school_id>/', views.chat_room_data, name='chat_room_data'),
     path('rooms/', views.chat_rooms, name='chat_rooms'),
     path('history/<str:room_name>/', views.chat_history, name='chat_history'),
+    path('clear/<str:room_name>/', views.clear_chat_messages, name='clear_chat_messages'),
+    path('send/<str:room_name>/', views.send_message, name='send_message'),
 ] 
